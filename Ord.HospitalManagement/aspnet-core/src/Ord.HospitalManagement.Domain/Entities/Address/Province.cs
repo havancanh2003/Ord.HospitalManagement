86441e9be19eda5ede82184e2b37cc5b0e288c36
canhhv <havancanh2003@gmail.com>
2024-12-20 18:03:28 +0700

    base dự án abp

---
+++ Ord.HospitalManagement/aspnet-core/src/Ord.HospitalManagement.Domain/Entities/Address/Province.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities;
+
+namespace Ord.HospitalManagement.Entities.Address
+{
+    public class Province : Entity<int>
+    {
+        public string Code { get; set; } = null!;
+        public string Name { get; set; } = null!;
+    }
+}
